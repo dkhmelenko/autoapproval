98fabecba1cb66432d8be8877736d8f3bdb3d599
Dmytro Khmelenko <d.khmelenko@gmail.com>
2018-12-26 00:09:06 +0100

    Stop approval if PR already contains labels to be applied

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,34 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 import { PullRequestsCreateReviewParams } from '@octokit/rest'
+const getConfig = require('probot-config');
 
 export = (app: Application) => {
-  app.on(['pull_request.opened', 'pull_request.reopened'], async (context) => {
+  app.on(['pull_request.opened', 'pull_request.reopened', 'pull_request.labeled'], async (context) => {
     app.log(context)
 
     // reading configuration
-    const config = await context.config('autoapproval.yml')
+    const config = await getConfig(context, 'autoapproval.yml')
     context.log(config, 'Loaded config')
 
     const pr = context.payload.pull_request
+
+    // reading pull request labels for later check
+    // if pull request contains all labels to be added, 
+    // then the assumption is that the PR is already approved and no actions required
+    const prLabels = pr.labels.map((label: any) => label.name)
+    const labelsToAdd = config.apply_labels
+    const prHasAppliedLabels = labelsToAdd.every((label: String) => prLabels.includes(label))
+    context.log("PR labels: %s, config apply labels: %s, condition passed: %s", prLabels, labelsToAdd, prHasAppliedLabels)
+    if (prHasAppliedLabels) {
+      context.log("PR has already labels to be added after approval. The PR might be already approved.")
+      return
+    }
 
     // reading pull request owner info and check it with configuration
     const prUser = pr.user.login
     const ownerSatisfied = config.from_owner.length == 0 || config.from_owner.includes(prUser)
 
     // reading pull request labels and check them with configuration
-    const prLabels = pr.labels.map((label: any) => label.name)
     const missingRequiredLabels = config.required_labels
       .filter((requiredLabel: any) => !prLabels.includes(requiredLabel))
 
@@ -26,7 +38,6 @@ export = (app: Application) => {
       await context.github.pullRequests.createReview(prParams as PullRequestsCreateReviewParams)
 
       // if there are labels required to be added, add them
-      const labelsToAdd = config.apply_labels
       if (labelsToAdd.length > 0) {
         // trying to apply existing labels to PR. If labels ddn't exist, this call will fail
         await context.github.issues.addLabels(context.issue({ labels: labelsToAdd}))
@@ -36,5 +47,6 @@ export = (app: Application) => {
       // one of the ckecks failed
       context.log("Condition failed! \n - missing required labels: %s\n - PR owner found: %s", missingRequiredLabels, ownerSatisfied)
     }
+
   })
 }
