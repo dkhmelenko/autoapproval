79e6e1d0f2320da0a66af291248c884597251e76
Dmytro Khmelenko <d.khmelenko@gmail.com>
2019-11-11 21:57:48 +0100

    Implement checking approved PRs using GitHub API (#79)

--- test/index.test.ts
+++ test/index.test.ts
@@ -22,67 +22,7 @@ describe('Autoapproval bot', () => {
     app.app = () => 'test'
   })
 
-  test('PR has already applied labels and should do nothing', async (done) => {
-    const payload = require('./fixtures/pull_request.opened.json')
-    const config = btoa('from_owner: []\nrequired_labels: []\nblacklisted_labels: []\napply_labels:  \n- merge')
-
-    nock('https://api.github.com')
-      .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
-      .reply(200, { content: config })
-
-    nock('https://api.github.com')
-      .post('/repos/dkhmelenko/autoapproval/pulls/1/reviews', (body: any) => {
-        throw new Error('PR might be already approved, no need to approve again')
-      })
-      .reply(200)
-
-    // Receive a webhook event
-    await probot.receive({ name: 'pull_request', payload })
-    done()
-    nock.cleanAll()
-  })
-
-  test('PR has required labels and owner satisfied - will be approved', async (done) => {
-    const payload = require('./fixtures/pull_request.opened.json')
-    const config = btoa('from_owner:\n  - dkhmelenko\nrequired_labels:\n  - merge\nblacklisted_labels: []\napply_labels: []')
-
-    nock('https://api.github.com')
-      .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
-      .reply(200, { content: config })
-
-    nock('https://api.github.com')
-      .post('/repos/dkhmelenko/autoapproval/pulls/1/reviews', (body: any) => {
-        return body.event === 'APPROVE'
-      })
-      .reply(200)
-
-    // Receive a webhook event
-    await probot.receive({ name: 'pull_request', payload })
-    done()
-    nock.cleanAll()
-  })
-
-  test('PR blacklisted labels entry missing - will be approved', async (done) => {
-    const payload = require('./fixtures/pull_request.opened.json')
-    const config = btoa('from_owner:\n  - dkhmelenko\nrequired_labels:\n  - merge\napply_labels: []')
-
-    nock('https://api.github.com')
-      .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
-      .reply(200, { content: config })
-
-    nock('https://api.github.com')
-      .post('/repos/dkhmelenko/autoapproval/pulls/1/reviews', (body: any) => {
-        return body.event === 'APPROVE'
-      })
-      .reply(200)
-
-    // Receive a webhook event
-    await probot.receive({ name: 'pull_request', payload })
-    done()
-    nock.cleanAll()
-  })
-
-  test('PR has blacklisted labels - will NOT be approved', async (done) => {
+  test('PR has blacklisted labels -> will NOT be approved', async (done) => {
     const payload = require('./fixtures/pull_request.opened.json')
     const config = btoa('from_owner:\n  - dkhmelenko\nrequired_labels:\n  - merge\nblacklisted_labels:\n  - wip\napply_labels: []')
 
@@ -90,19 +30,13 @@ describe('Autoapproval bot', () => {
       .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
       .reply(200, { content: config })
 
-    nock('https://api.github.com')
-      .post('/repos/dkhmelenko/autoapproval/pulls/1/reviews', (body: any) => {
-        throw new Error('PR should not be approved in this case!')
-      })
-      .reply(200)
-
     // Receive a webhook event
     await probot.receive({ name: 'pull_request', payload })
     done()
     nock.cleanAll()
   })
 
-  test('PR satisfies owner, has no required labels - will NOT be approved', async (done) => {
+  test('PR has no required labels -> will NOT be approved', async (done) => {
     const payload = require('./fixtures/pull_request.opened.json')
     const config = btoa('from_owner:\n  - dkhmelenko\nrequired_labels:\n  - ready\nblacklisted_labels: []\napply_labels: []')
 
@@ -110,19 +44,13 @@ describe('Autoapproval bot', () => {
       .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
       .reply(200, { content: config })
 
-    nock('https://api.github.com')
-      .post('/repos/dkhmelenko/autoapproval/pulls/1/reviews', (body: any) => {
-        throw new Error('PR should not be approved in this case!')
-      })
-      .reply(200)
-
     // Receive a webhook event
     await probot.receive({ name: 'pull_request', payload })
     done()
     nock.cleanAll()
   })
 
-  test('PR has no owner, has required labels - will NOT be approved', async (done) => {
+  test('PR has no expected owner -> will NOT be approved', async (done) => {
     const payload = require('./fixtures/pull_request.opened.json')
     const config = btoa('from_owner:\n  - blabla\nrequired_labels:\n  - merge\nblacklisted_labels: []\napply_labels: []')
 
@@ -130,9 +58,28 @@ describe('Autoapproval bot', () => {
       .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
       .reply(200, { content: config })
 
+    // Receive a webhook event
+    await probot.receive({ name: 'pull_request', payload })
+    done()
+    nock.cleanAll()
+  })
+
+  test('PR has required labels and expected owner -> will be approved', async (done) => {
+    const payload = require('./fixtures/pull_request.opened.json')
+    const config = btoa('from_owner:\n  - dkhmelenko\nrequired_labels:\n  - merge\nblacklisted_labels: []\napply_labels: []')
+    const reviews = require('./fixtures/pull_request_reviews_empty.json')
+
+    nock('https://api.github.com')
+      .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
+      .reply(200, { content: config })
+
+    nock('https://api.github.com')
+      .get('/repos/dkhmelenko/autoapproval/pulls/1/reviews')
+      .reply(200, reviews)
+
     nock('https://api.github.com')
       .post('/repos/dkhmelenko/autoapproval/pulls/1/reviews', (body: any) => {
-        throw new Error('PR should not be approved in this case!')
+        return body.event === 'APPROVE'
       })
       .reply(200)
 
@@ -142,13 +89,18 @@ describe('Autoapproval bot', () => {
     nock.cleanAll()
   })
 
-  test('PR approved, label is applied', async (done) => {
+  test('PR approved and label is applied', async (done) => {
     const payload = require('./fixtures/pull_request.opened.json')
     const config = btoa('from_owner:\n  - dkhmelenko\nrequired_labels: []\nblacklisted_labels: []\napply_labels:\n  - done')
+    const reviews = require('./fixtures/pull_request_reviews_empty.json')
 
     nock('https://api.github.com')
       .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
       .reply(200, { content: config })
+
+    nock('https://api.github.com')
+      .get('/repos/dkhmelenko/autoapproval/pulls/1/reviews')
+      .reply(200, reviews)
 
     nock('https://api.github.com')
       .post('/repos/dkhmelenko/autoapproval/pulls/1/reviews', (body: any) => {
@@ -168,7 +120,26 @@ describe('Autoapproval bot', () => {
     nock.cleanAll()
   })
 
-  test('Autoapproval review was dismissed, approve PR again', async (done) => {
+  test('PR is already approved -> will not be approved again', async (done) => {
+    const payload = require('./fixtures/pull_request_review.dismissed.json')
+    const config = btoa('from_owner:\n  - dkhmelenko\nrequired_labels: []\nblacklisted_labels: []\napply_labels:\n  - merge')
+    const reviews = require('./fixtures/pull_request_reviews.json')
+
+    nock('https://api.github.com')
+      .get('/repos/dkhmelenko/autoapproval/contents/.github/autoapproval.yml')
+      .reply(200, { content: config })
+
+    nock('https://api.github.com')
+      .get('/repos/dkhmelenko/autoapproval/pulls/1/reviews')
+      .reply(200, reviews)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'pull_request_review.dismissed', payload })
+    done()
+    nock.cleanAll()
+  })
+
+  test('Autoapproval review was dismissed -> approve PR again', async (done) => {
     const payload = require('./fixtures/pull_request_review.dismissed.json')
     const config = btoa('from_owner:\n  - dkhmelenko\nrequired_labels: []\nblacklisted_labels: []\napply_labels:\n  - merge')
     const reviews = require('./fixtures/pull_request_reviews.json')
